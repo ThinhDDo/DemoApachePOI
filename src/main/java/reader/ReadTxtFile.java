2b25dfdd3a2922785d35cf5aff7ca0568e8d0c75
ThinhDDo <doduythinhfa.1999@gmail.com>
2020-04-24 14:40:46 +0700

    Add file ReadTxtFile.java

---
+++ src/main/java/reader/ReadTxtFile.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reader;
+
+import java.io.File;
+
+/**
+ *
+ * @author doduy
+ */
+public class ReadTxtFile {
+    private File rfile;
+    private File wfile;
+}
